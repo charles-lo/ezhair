6537796bce8e844dfe995d3a45f91f03733333dc
Charles Lo <jerries.lo@gmail.com>
2015-05-16 19:06:48 +0800

    1. landing page

---
+++ com.ezhair/src/com/ezhair/MainActivity.java
@@ -0,0 +1,60 @@
+package com.ezhair;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+public class MainActivity extends FragmentActivity {
+	FragmentTransaction ft = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		Fresco.initialize(MainActivity.this);
+		setContentView(R.layout.activity_main);
+
+		new PageHome();
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction().add(R.id.container, new PageHome()).commit();
+		}
+		// Uri uri =
+		// Uri.parse("http://upload.wikimedia.org/wikipedia/commons/5/53/Zhuangzong_of_Later_Tang.jpg");
+		// SimpleDraweeView draweeView = (SimpleDraweeView)
+		// findViewById(R.id.my_image_view);
+		// draweeView.setImageURI(uri);
+		// draweeView.invalidate();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void replaceFragment(Fragment newFragment) {
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		ft.replace(R.id.container, newFragment);
+		ft.addToBackStack(null);
+		ft.commit();
+	}
+}
---
+++ com.ezhair/src/com/ezhair/PageHome.java
@@ -0,0 +1,43 @@
+package com.ezhair;
+
+import com.ezhair.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PageHome extends Fragment {
+	static private final int MESSAGE_STATE_LAUNCH = 1000;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.page_home, container, false);
+		final View notification = rootView.findViewById(R.id.notification);
+		final View hot = rootView.findViewById(R.id.hot);
+		final View near = rootView.findViewById(R.id.near);
+		final View search = rootView.findViewById(R.id.search);
+		final View mail = rootView.findViewById(R.id.mail);
+
+		Handler handler = new Handler() {
+			public void handleMessage(Message msg) {
+				switch (msg.what) {
+				case MESSAGE_STATE_LAUNCH: {
+					notification.setVisibility(View.VISIBLE);
+					hot.setVisibility(View.VISIBLE);
+					near.setVisibility(View.VISIBLE);
+					search.setVisibility(View.VISIBLE);
+					mail.setVisibility(View.VISIBLE);
+					break;
+				}
+				}
+				super.handleMessage(msg);
+			}
+		};
+		handler.sendEmptyMessageDelayed(MESSAGE_STATE_LAUNCH, 1000);
+		return rootView;
+	}
+}
